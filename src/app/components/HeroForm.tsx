04167f1dcfb904be4d9af735d5d75089c39cd9d0
Luciano <lucianorieth@outlook.com>
2024-10-31 10:11:09 -0300

    Hero form

---
+++ src/app/components/HeroForm.tsx
@@ -0,0 +1,75 @@
+"use client";
+
+import { useRouter } from 'next/navigation';
+import { useForm, SubmitHandler } from 'react-hook-form';
+import { useEffect, useState } from 'react';
+import { Hero } from '@/types/hero';
+import { CheckIcon } from '@heroicons/react/24/solid';
+import { fetchHero, saveHero } from '@/services/heroService';
+import { fetchCategories } from '@/services/categoryService';
+import { Category } from '@/types/category';
+
+interface HeroFormInputs {
+  name: string;
+  categoryId: number;
+}
+
+const HeroForm: React.FC<{ heroId: string | null }> = ({ heroId }) => {
+  const router = useRouter(); 
+  const { register, handleSubmit, setValue, formState: { errors } } = useForm<HeroFormInputs>();
+  const [categories, setCategories] = useState<Category[]>([]);
+
+  useEffect(() => {
+    fetchCategories().then((response) => setCategories(response.Items));
+    if (heroId) {
+      fetchHero(Number(heroId)).then((response) => {
+        const hero = response;
+        setValue('name', hero.Name);
+        setValue('categoryId', hero.Category.Id);
+      });
+    }
+  }, [heroId, setValue]);
+
+  const onSubmit: SubmitHandler<HeroFormInputs> = async (data: any) => {
+    await saveHero(Number(heroId), data as Hero);
+    router.push('/heroes'); 
+  };
+
+  return (
+    <form onSubmit={handleSubmit(onSubmit)} className="space-y-4">
+      <div>
+        <label className="block text-gray-700 mb-1">Nome do Herói</label>
+        <input
+          {...register('name', { required: "O nome é obrigatório" })}
+          className={`w-full p-2 border rounded mt-1 focus:outline-none ${errors.name ? 'border-red-500' : 'border-gray-300 focus:border-blue-500'}`}
+          placeholder="Nome"
+        />
+        {errors.name && <p className="text-red-500 text-sm mt-1">{errors.name.message}</p>}
+      </div>
+
+      <div>
+        <label className="block text-gray-700 mb-1">Categoria</label>
+        <select
+          {...register('categoryId', { required: "Selecione uma categoria" })}
+          className={`w-full p-2 border rounded mt-1 focus:outline-none ${errors.categoryId ? 'border-red-500' : 'border-gray-300 focus:border-blue-500'}`}
+          defaultValue=""
+        >
+          <option value="" disabled>Selecione uma categoria</option>
+          {categories.map((category) => (
+            <option key={category.Id} value={category.Id}>{category.Name}</option>
+          ))}
+        </select>
+        {errors.categoryId && <p className="text-red-500 text-sm mt-1">{errors.categoryId.message}</p>}
+      </div>
+
+      <button
+        type="submit"
+        className="flex items-center justify-center w-full px-4 py-2 bg-green-500 text-white font-semibold rounded-full shadow-md hover:bg-green-600 transition-all duration-300"
+      >
+        <CheckIcon className="h-5 w-5 mr-2" /> {heroId ? "Atualizar" : "Salvar"}
+      </button>
+    </form>
+  );
+};
+
+export default HeroForm;
--- src/app/heroes/create/page.tsx
+++ src/app/heroes/create/page.tsx
@@ -1,82 +1,9 @@
 "use client";
 
+import { Suspense, useEffect, useState } from 'react';
 import { useRouter, useSearchParams } from 'next/navigation';
-import { useForm, SubmitHandler } from 'react-hook-form';
-import { useEffect, useState } from 'react';
-import { Hero } from '@/types/hero';
-import { ArrowLeftIcon, CheckIcon } from '@heroicons/react/24/solid';
-import { fetchHero, saveHero } from '@/services/heroService';
-import { fetchCategories } from '@/services/categoryService';
-import { Category } from '@/types/category';
-
-interface HeroFormInputs {
-  name: string;
-  categoryId: number;
-}
-
-const HeroForm: React.FC<{ heroId: string | null }> = ({ heroId }) => {
-  const router = useRouter();
-  const { register, handleSubmit, setValue, formState: { errors } } = useForm<HeroFormInputs>();
-  const [categories, setCategories] = useState<Category[]>([]);
-
-  useEffect(() => {
-    fetchCategories().then((response) => setCategories(response.Items));
-    if (heroId) {
-      fetchHero(Number(heroId)).then((response) => {
-        const hero = response;
-        setValue('name', hero.Name);
-        setValue('categoryId', hero.Category.Id);
-      });
-    }
-  }, [heroId, setValue]);
-
-  const onSubmit: SubmitHandler<HeroFormInputs> = async (data: any) => {
-    await saveHero(Number(heroId), data as Hero);
-    router.push('/heroes');
-  };
-
-  return (
-    <form onSubmit={handleSubmit(onSubmit)} className="space-y-4">
-      <div>
-        <label className="block text-gray-700 mb-1">Nome do Herói</label>
-        <input
-          {...register('name', { required: "O nome é obrigatório" })}
-          className={`w-full p-2 border rounded mt-1 focus:outline-none ${errors.name ? 'border-red-500' : 'border-gray-300 focus:border-blue-500'
-            }`}
-          placeholder="Nome"
-        />
-        {errors.name && (
-          <p className="text-red-500 text-sm mt-1">{errors.name.message}</p>
-        )}
-      </div>
-
-      <div>
-        <label className="block text-gray-700 mb-1">Categoria</label>
-        <select
-          {...register('categoryId', { required: "Selecione uma categoria" })}
-          className={`w-full p-2 border rounded mt-1 focus:outline-none ${errors.categoryId ? 'border-red-500' : 'border-gray-300 focus:border-blue-500'
-            }`}
-          defaultValue=""
-        >
-          <option value="" disabled>Selecione uma categoria</option>
-          {categories.map((category) => (
-            <option key={category.Id} value={category.Id}>{category.Name}</option>
-          ))}
-        </select>
-        {errors.categoryId && (
-          <p className="text-red-500 text-sm mt-1">{errors.categoryId.message}</p>
-        )}
-      </div>
-
-      <button
-        type="submit"
-        className="flex items-center justify-center w-full px-4 py-2 bg-green-500 text-white font-semibold rounded-full shadow-md hover:bg-green-600 transition-all duration-300"
-      >
-        <CheckIcon className="h-5 w-5 mr-2" /> {heroId ? "Atualizar" : "Salvar"}
-      </button>
-    </form>
-  );
-};
+import { ArrowLeftIcon } from '@heroicons/react/24/solid';
+import HeroForm from "@/app/components/HeroForm";
 
 const CreateHero: React.FC = () => {
   const router = useRouter();
@@ -84,7 +11,6 @@ const CreateHero: React.FC = () => {
   const [heroId, setHeroId] = useState<string | null>(null);
 
   useEffect(() => {
-    // Capture 'id' from searchParams safely and update heroId
     const id = searchParams?.get('id');
     setHeroId(id || "");
   }, [searchParams]);
@@ -100,6 +26,7 @@ const CreateHero: React.FC = () => {
       </h1>
 
       <div className="w-full max-w-lg bg-white rounded-lg shadow-lg p-6">
+        
         <div className="flex justify-between items-center mb-4">
           <button
             onClick={handleBack}
@@ -109,8 +36,10 @@ const CreateHero: React.FC = () => {
           </button>
         </div>
 
-        {/* Render HeroForm only after heroId is set */}
-        {heroId !== null && <HeroForm heroId={heroId} />}
+      
+        <Suspense fallback={<div>Carregando...</div>}>
+          {heroId !== null && <HeroForm heroId={heroId} />}
+        </Suspense>
       </div>
     </div>
   );
